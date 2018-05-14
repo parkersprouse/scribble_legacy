37cc874c59522f47b909a7dff683be943657b85f
Parker Sprouse <parker@parkersprouse.me>
2018-05-13 23:17:53 -0500

    Added SCSS stylesheet

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,13 @@
+import Buefy from 'buefy';
 import Vue from 'vue';
+import 'buefy/lib/buefy.css';
+
 import App from './App.vue';
 import router from './router';
+import './assets/styles.scss';
 
 Vue.config.productionTip = false;
+Vue.use(Buefy);
 
 new Vue({
   router,
