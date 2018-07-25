ad691d1826f75869310e3e92e6a12500dba4d950
Parker Sprouse <parker@parkersprouse.me>
2018-07-25 18:17:04 -0500

    Added the hamburgers css library to use

--- client/src/main.js
+++ client/src/main.js
@@ -8,6 +8,7 @@ import Navbar from './components/Navbar.vue';
 import Footer from './components/Footer.vue';
 import './assets/styles/styles.scss';
 import './assets/styles/materialdesignicons.min.css';
+import './assets/styles/hamburgers.css';
 
 Vue.config.productionTip = false;
 Vue.use(Buefy);
--- client/src/pages/ShowScribblePage.vue
+++ client/src/pages/ShowScribblePage.vue
@@ -13,7 +13,7 @@
     <div v-else class='container'>
       <div class='content'>
         <h2 class='title is-2 is-spaced has-text-centered'>{{ scribble.title }}</h2>
-        <blockquote>{{ scribble.body }}</blockquote>
+        <div class='scribble-body'>{{ scribble.body }}</div>
       </div>
     </div>
 
@@ -41,3 +41,21 @@ export default {
   }
 };
 </script>
+
+<style>
+.scribble-body {
+  white-space: pre-wrap;
+  padding: 1.25rem;
+  border: 1px solid #a1a1a1;
+  border-radius: 5px;
+  /* background-color: #f5f5f5; */
+}
+
+@media screen and (min-width: 1024px) {
+  .scribble-body {
+    max-width: 50%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+</style>
